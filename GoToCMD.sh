09a5ff7cb9ca8f38261147ef39d4c5bd717e98c4
Thesk0rn <47185037+Thesk0rn@users.noreply.github.com>
2019-02-12 10:07:22 +0100

    Update GoToCMD.sh
    Verification lancement en root
    Verficaition make installé

--- GoToCMD.sh
+++ GoToCMD.sh
@@ -1,4 +1,16 @@
-echo Vous devez exécuter ce script en mode sudo
+#!/bin/bash
+
+if [ $(whoami) != "root" ]
+then
+	echo -e "Vous devez exécuter ce script en mode sudo"
+	exit 1
+fi
+
+if [[ -z "$(dpkg -s make | grep Status)" ]]
+	then
+		echo -e "Veuillez installer make pour executer ce script"
+        exit 1
+fi
 
 cp MMF /bin
 mv MMF /sbin
